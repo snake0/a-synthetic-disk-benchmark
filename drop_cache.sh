e03f79dd81d4d28dedd257bf479fb5b5ca6d7353
tonysuo <suokunstar@gmail.com>
2016-04-17 19:33:47 -0600

    drop_cache.sh

---
+++ drop_cache.sh
@@ -0,0 +1,11 @@
+echo 3 > /proc/sys/vm/drop_caches
+echo "finish cleaning cache"
+#./frread /mnt/part0/file-1G-1 64 &
+#/apps/dos/fread /apps/tmp 64
+#./fread /mnt/part0/file-1G 64 &
+#./fread /mnt/part0/file-1G-1 64 &
+#./frread /mnt/part0/file-1G-1 64 &
+#./frread /mnt/part0/file-1G-1 64 &
+#./frread /mnt/part0/file-256m 64 &
+#./frread /mnt/part0/file-64m 64 &
+#./frread /mnt/part0/file-128m 64 &
